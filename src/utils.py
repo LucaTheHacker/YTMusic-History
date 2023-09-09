d16d3a740a2c10d202b2598c70f5c318e8b333d5
damettoluca <github@damettoluca.com>
2023-09-10 01:22:36 +0200

    Fixed utils file count in download folder

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,20 @@
-import shutil
 import os
 
 
 def get_dir_size(path='.'):
     total = 0
+    count = 0
     with os.scandir(path) as it:
         for entry in it:
             if entry.is_file():
                 total += entry.stat().st_size
+                count += 1
             elif entry.is_dir():
                 total += get_dir_size(entry.path)
-    return total
+    return total, count
 
 
 def get_fs_stats():
-    download_folder_size = get_dir_size('/downloads')
-    download_folder_filecount = len([name for name in os.listdir('/downloads') if os.path.isfile(name)])
+    download_folder_size, download_folder_filecount = get_dir_size('/downloads')
     database_size = os.path.getsize('/database/database.db')
     return download_folder_size, download_folder_filecount, database_size
