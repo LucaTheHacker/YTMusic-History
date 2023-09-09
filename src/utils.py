1d8ef144683cd773c9333d49d6aadec6bf3c3752
damettoluca <github@damettoluca.com>
2023-09-10 00:38:20 +0200

    Fixed util to get directory size

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,19 @@ import shutil
 import os
 
 
+def get_dir_size(path='.'):
+    total = 0
+    with os.scandir(path) as it:
+        for entry in it:
+            if entry.is_file():
+                total += entry.stat().st_size
+            elif entry.is_dir():
+                total += get_dir_size(entry.path)
+    return total
+
+
 def get_fs_stats():
-    _, download_folder_size, _ = shutil.disk_usage("/downloads")
+    download_folder_size = get_dir_size('/downloads')
     download_folder_filecount = len([name for name in os.listdir('/downloads') if os.path.isfile(name)])
     database_size = os.path.getsize('/database/database.db')
     return download_folder_size, download_folder_filecount, database_size
